8380b3b454d02207e99be967cc84dd9af626631e
marcel wefuan <marctah.2020@gmail.com>
2023-05-02 19:39:37 -0400

    adding web server infra

---
+++ var.tf
@@ -0,0 +1,13 @@
+variable "AMI_ID" {
+   type = string
+   description = "AMI ID"
+   default =   "ami-02396cdd13e9a1257"
+}
+
+
+
+variable "INSTANCE_TYPE" {
+   type = string
+   description = "type of EC2 instance"
+   default =   "t2.micro"
+}
